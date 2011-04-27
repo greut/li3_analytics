48ca5828063fc4e6fc0d7b7409a5b7f17fcc38e1
Yoan Blanc <yoan@dosimple.ch>
2011-04-27 17:14:12 +0200

    Move the HTML into a proper view.

--- extensions/helper/Analytics.php
+++ extensions/helper/Analytics.php
@@ -3,33 +3,43 @@
 namespace li3_analytics\extensions\helper;
 
 use li3_analytics\extensions\Trackings;
+use lithium\template\View;
+use lithium\g11n\Message;
 
 class Analytics extends \lithium\template\Helper
 {
+	protected $_view;
+
 	// http://code.google.com/intl/fr/apis/analytics/docs/tracking/asyncTracking.html
 	function script($tracking='default')
 	{
 		$tracking = Trackings::get($tracking);
-		$commands = array_map(function($item) {
-			return '_gaq.push('.json_encode($item).')';
-		}, $tracking->commands());
-		$commands = implode(';', $commands);
+		$class = get_class($tracking);
+		$adapter = mb_substr($class, mb_strrpos($class, '\\')+1);
+		$template = mb_strtolower($adapter);
+		$library = 'li3_analytics';
+		$view = $this->build_view();
 
-		return <<<EOS
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  {$commands}
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-EOS;
+		return $view->render(
+			'element',
+			compact('tracking'),
+			compact('template', 'library')
+		);
 	}
 
 	// TODO:
 	// http://code.google.com/intl/fr/apis/analytics/docs/gaJS/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._trackPageview
+
+	protected function build_view()
+	{
+		if (!isset($this->_view)) {
+			$this->_view = new View(array(
+				'paths' => array(
+					'element' => '{:library}/views/elements/{:template}.{:type}.php',
+				),
+				'outputFilters' => Message::aliases(),
+			));
+		}
+		return $this->_view;
+	}
 }
---
+++ views/elements/googleanalytics.html.php
@@ -0,0 +1,7 @@
+<?php
+$commands = array_map(function($item) {
+	return '_gaq.push('.json_encode($item).');';
+}, $tracking->commands());
+$commands = implode('', $commands);
+?>
+<script type="text/javascript">var _gaq=_gaq||[];<?php echo $commands ?>(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script>
