316001ed8b25fd41a6f0a3a43152710d3b4f654a
Yoan Blanc <yoan@dosimple.ch>
2011-05-15 17:11:40 +0200

    Fail silently if not configured

--- extensions/helper/Analytics.php
+++ extensions/helper/Analytics.php
@@ -5,6 +5,8 @@ namespace li3_analytics\extensions\helper;
 use li3_analytics\extensions\Trackings;
 use lithium\template\View;
 use lithium\g11n\Message;
+use lithium\core\Environment;
+use lithium\core\ConfigException;
 
 class Analytics extends \lithium\template\Helper
 {
@@ -12,18 +14,24 @@ class Analytics extends \lithium\template\Helper
 
 	// http://code.google.com/intl/fr/apis/analytics/docs/tracking/asyncTracking.html
 	function script() {
-		$tracking = Trackings::get();
-		$class = get_class($tracking);
-		$adapter = mb_substr($class, mb_strrpos($class, '\\')+1);
-		$template = mb_strtolower($adapter);
-		$library = 'li3_analytics';
-		$view = $this->build_view();
+		try {
+			$tracking = Trackings::get();
+			$class = get_class($tracking);
+			$adapter = mb_substr($class, mb_strrpos($class, '\\')+1);
+			$template = mb_strtolower($adapter);
+			$library = 'li3_analytics';
+			$view = $this->build_view();
 
-		return $view->render(
-			'element',
-			compact('tracking'),
-			compact('template', 'library')
-		);
+			return $view->render(
+				'element',
+				compact('tracking'),
+				compact('template', 'library')
+			);
+		} catch (ConfigException $e) {
+			if (!Environment::is('production')) {
+				return '<!-- li3_analytics: '.$e->getMessage().' -->';
+			}
+		}
 	}
 
 	// TODO:
